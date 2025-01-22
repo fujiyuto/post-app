d5f659a687df78696b23c2cf0d6a5a81190edb9d
fujiyuto <yuto.fujiwara.itfarm@gmail.com>
2025-01-23 00:51:18 +0900

    店テーブルに席時間・予約可能フラグ・上限客数のカラム追加

--- database/factories/RestaurantFactory.php
+++ database/factories/RestaurantFactory.php
@@ -17,14 +17,25 @@ class RestaurantFactory extends Factory
     public function definition(): array
     {
         return [
-            'restaurant_name' => fake()->realText(10),
-            'zip_cd'          => fake()->postcode(),
-            'address'         => fake()->prefecture() . fake()->city(),
-            'address_detail'  => fake()->streetAddress(),
-            'email'           => fake()->safeEmail(),
-            'tel_no'          => fake()->phoneNumber(),
-            'price_min'       => 1000,
-            'price_max'       => 4000
+            'restaurant_name'  => fake()->realText(10),
+            'zip_cd'           => fake()->postcode(),
+            'address'          => fake()->prefecture() . fake()->city(),
+            'address_detail'   => fake()->streetAddress(),
+            'email'            => fake()->safeEmail(),
+            'tel_no'           => fake()->phoneNumber(),
+            'price_min'        => 1000,
+            'price_max'        => 4000,
+            'seating_duration' => $this->randomSeatingDuration(),
+            'is_reservable'    => true,
+            'capacity'         => random_int(1, 20)
         ];
+    }
+
+    private function randomSeatingDuration(): float
+    {
+        $random_int = random_int(0, 9);
+        $decimal_arr = [0, 0.25, 0.5, 0.75];
+        $random_decimal = $decimal_arr[random_int(0, 3)];
+        return $random_int + $random_decimal;
     }
 }
--- database/migrations/2024_08_18_084033_create_restaurants_table.php
+++ database/migrations/2024_08_18_084033_create_restaurants_table.php
@@ -22,7 +22,10 @@ return new class extends Migration
             $table->unsignedInteger('price_min')->nullable()->comment('価格（最小）');
             $table->unsignedInteger('price_max')->nullable()->comment('価格（最大）');
             $table->integer('post_num')->default(0)->comment('投稿数');
-            $table->float('point_avg')->default(0)->comment('平均点数');
+            $table->float('point_avg', 2, 1)->default(0)->comment('平均点数');
+            $table->float('seating_duration', 4, 2)->default(1.0)->comment('席時間');
+            $table->boolean('is_reservable')->default(false)->comment('予約可能');
+            $table->integer('capacity')->unsigned()->default(0)->comment('上限客数');
             $table->timestamps();
         });
     }
